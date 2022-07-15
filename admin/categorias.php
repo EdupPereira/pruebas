190c47261f1b33745ef0f9929186ee5a809c18c6
EdupPereira <106452653+EdupPereira@users.noreply.github.com>
2022-07-15 14:56:53 -0500

    Update categorias.php

--- admin/categorias.php
+++ admin/categorias.php
@@ -4,100 +4,100 @@ include('includes/adminheader.php');
 include ('includes/adminnav.php');
 
 
-	if (isset($_POST['icategoria'])) {
-		$codigo_categoriat=$_POST['codigo_categoriat'];
-		$descripcion_categoriat=$_POST['descripcion_categoriat'];
+if (isset($_POST['icategoria'])) {
+	$codigo_categoriat=$_POST['codigo_categoriat'];
+	$descripcion_categoriat=$_POST['descripcion_categoriat'];
 
 
-		$query = "INSERT INTO categoria_transparencia(codigo_categoriat,descripcion_categoriat) VALUES ('$codigo_categoriat','$descripcion_categoriat')";
-		$result = pg_query($query);
-		if (pg_affected_rows($result) > 0) {
-			echo '<script>
-			swal("Buen Trabajo!", " La categoria se registro con éxito", "success");
-			</script>';
-		}
-		else {
-			echo '<script>swal("ERROR!", "Lo sentimos ocurrió un error al registrar la categoría", "error");</script>';
-		}
+	$query = "INSERT INTO categoria_transparencia(codigo_categoriat,descripcion_categoriat) VALUES ('$codigo_categoriat','$descripcion_categoriat')";
+	$result = pg_query($query);
+	if (pg_affected_rows($result) > 0) {
+		echo '<script>
+		swal("Buen Trabajo!", " La categoria se registro con éxito", "success");
+		</script>';
+	}
+	else {
+		echo '<script>swal("ERROR!", "Lo sentimos ocurrió un error al registrar la categoría", "error");</script>';
+	}
+}
+
+if(isset($_POST['editarCat'])) {
+
+	$codigo_cat_editar = $_POST['codigo_categoriat'];
+	$descripcion_editar = $_POST['nombre_area'];
+
+	$editarCat = "UPDATE categoria_transparencia SET codigo_categoriat = '$nombre_area_editar' WHERE codigo_categoriat = '$codigo_area_editar'";
+
+	$resultado = pg_query($editarCat);
+	if (pg_affected_rows($resultado) > 0 ) {
+		echo '
+		<script>
+		swal("Buen Trabajo!", "El area se edito con éxito", "success");
+		</script>';
 	}
 
-	if(isset($_POST['editarArea'])) {
+	else {
+		echo '<script>swal("ERROR!", "Lo sentimos ocurrió un error al editar el area", "error");</script>';
+	}
+} 
 
-		$codigo_area_editar = $_POST['codigo_categoriat'];
-		$nombre_area_editar = $_POST['nombre_area'];
-		
-		$editarRol = "UPDATE area SET nombre_area = '$nombre_area_editar' WHERE codigo_categoriat = '$codigo_area_editar'";
-
-		$resultado = pg_query($editarRol);
-		if (pg_affected_rows($resultado) > 0 ) {
-			echo '
-			<script>
-			swal("Buen Trabajo!", "El area se edito con éxito", "success");
-			</script>';
-		}
-
-		else {
-			echo '<script>swal("ERROR!", "Lo sentimos ocurrió un error al editar el area", "error");</script>';
-		}
-	} 
-
-	if(isset($_POST['elimina_cat'])) {
-		$codigo_cat =$_POST['elimina_cat'];
-		$del_query = "DELETE FROM categoria_transparencia WHERE codigo_categoriat='$codigo_cat'";
-		$run_del_query = pg_query($del_query);
-		if (pg_affected_rows($run_del_query) > 0) {
-			echo '<script>
-			swal("Buen Trabajo!", "La categoría se Elimino con éxito", "success");
-			</script>';
-		}
-		else {
-			echo '<script>swal("ERROR!", "Lo sentimos ocurrió un error al eliminar la categoría porque esta ligada a alguna subcategoria  ¿Que puede hacer? debe eliminar primero las subcategorias asociadas o no se podra eliminar", "error").then(function() {
-				window.location.replace("categorias.php");}); </script>';    
+if(isset($_POST['elimina_cat'])) {
+	$codigo_cat =$_POST['elimina_cat'];
+	$del_query = "DELETE FROM categoria_transparencia WHERE codigo_categoriat='$codigo_cat'";
+	$run_del_query = pg_query($del_query);
+	if (pg_affected_rows($run_del_query) > 0) {
+		echo '<script>
+		swal("Buen Trabajo!", "La categoría se Elimino con éxito", "success");
+		</script>';
+	}
+	else {
+		echo '<script>swal("ERROR!", "Lo sentimos ocurrió un error al eliminar la categoría porque esta ligada a alguna subcategoria  ¿Que puede hacer? debe eliminar primero las subcategorias asociadas o no se podra eliminar", "error").then(function() {
+			window.location.replace("categorias.php");}); </script>';    
 		}
 	} 
 
-?>
-<!-- Content Row -->
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-4"></div>
-		<div class="card mb-3">
-			<div class="card-header">
-				<!-- Button trigger modal -->
-				<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalGlosario">
-					Añadir Categoría
-				</button>
-			</div>
-			
-			<!-- Modal Glosario -->
-			<div class="modal fade" id="modalGlosario" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-				<div class="modal-dialog">
-					<div class="modal-content">
-						<div class="modal-header">
-							<h5 class="modal-title" id="exampleModalLabel">Añadir Categoría</h5>
-						</div>
-						<div class="modal-body">
-							<form  action="" method="post" class="form-inline" >
-								<input type="text"  name="codigo_categoriat" class="form-control col-md-12" placeholder="Codigo Categoría" >
-								<input type="text"  name="descripcion_categoriat" class="form-control col-md-12" placeholder="Nombre Categoría" >
-								
+	?>
+	<!-- Content Row -->
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-4"></div>
+			<div class="card mb-3">
+				<div class="card-header">
+					<!-- Button trigger modal -->
+					<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalGlosario">
+						Añadir Categoría
+					</button>
+				</div>
 
-								<div class="modal-footer col-lg-12 col-xs-12 col-md-12 col-sm-12">
-									<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
-									<button type="submit" class="btn btn-success" name="icategoria">
-									Guardar</button>
-								</div>
+				<!-- Modal Glosario -->
+				<div class="modal fade" id="modalGlosario" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalLabel">Añadir Categoría</h5>
+							</div>
+							<div class="modal-body">
+								<form  action="" method="post" class="form-inline" >
+									<input type="text"  name="codigo_categoriat" class="form-control col-md-12" placeholder="Codigo Categoría" >
+									<input type="text"  name="descripcion_categoriat" class="form-control col-md-12" placeholder="Nombre Categoría" >
 
-							</form>
+
+									<div class="modal-footer col-lg-12 col-xs-12 col-md-12 col-sm-12">
+										<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+										<button type="submit" class="btn btn-success" name="icategoria">
+										Guardar</button>
+									</div>
+
+								</form>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-		<div class="col-md-4"></div>
+			<div class="col-md-4"></div>
 
-	</div>
-	
+		</div>
+
 		<div class="row">
 			<div class="col-lg-12">
 				<div class="table-responsive">
@@ -151,24 +151,24 @@ include ('includes/adminnav.php');
 					</table>
 				</div>
 			</div>
-		
-	</div> 
-</div><!-- DIV QUE CIERRA EL CONTENEDOR DEL NAV -->
-<!-- MODAL PARA EDITAR AREA-->
-<div class="modal fade" id="editCat" tabindex="-1" role="dialog" aria-labelledby="editItemModalLabel" aria-hidden="true">
-	<div class="modal-dialog" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h5 class="modal-title" id="addItemModalLabel">Editar Categoria</h5>
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-				</button>
-			</div>
-			<div class="modal-body">
-				<form  action="" method="POST" class="form-inline">
-					<input type="text"  name="codigo_categoriat" class="form-control col-md-12" placeholder="Codigo Categoría" >
-					<input type="text"  name="descripcion_categoriat" class="form-control col-md-12" placeholder="Nombre Categoría" >
-					
+
+		</div> 
+	</div><!-- DIV QUE CIERRA EL CONTENEDOR DEL NAV -->
+	<!-- MODAL PARA EDITAR AREA-->
+	<div class="modal fade" id="editCat" tabindex="-1" role="dialog" aria-labelledby="editItemModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="addItemModalLabel">Editar Categoria</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<form  action="" method="POST" class="form-inline">
+						<input type="text"  name="codigo_categoriat" id="codigo_categoriat" class="form-control col-md-12" placeholder="Codigo Categoría" >
+						<input type="text"  name="descripcion_categoriat" id="descripcion_categoriat" class="form-control col-md-12" placeholder="Nombre Categoría" >
+
 						<br>
 						<div class="modal-footer col-lg-12 col-sm-12 col-md-12 col-xs-12">
 							<button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
@@ -208,11 +208,10 @@ $('#editCat').on('show.bs.modal', function (event) {
           var modal = $(this);
           modal.find('.modal-body #codigo_categoriat').val(codigo_categoriat);
           modal.find('.modal-body #descripcion_categoriat').val(descripcion_categoriat);
-         
+        
 
       });
 
   </script>
 
   <?php include ('includes/adminfooter.php');?>
-						
